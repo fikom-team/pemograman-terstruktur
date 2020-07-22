ea644d02f5acab383cff66fc905cec69bfc22159
hananloser <hasyrawi@gmail.com>
2020-07-22 16:23:11 +0800

    adding example

--- example-1/main.cpp
+++ example-1/main.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+/**
+ * @hananloser
+ * @return void
+ * **/
+void biodata()
+{
+    string name;
+    string alamat;
+    char jk;
+
+    cout << "Masukan Nama =";
+    // Masukan Variable Name
+    cin >> name;
+    // Tampil kan pesan di terminaljkjkjkjk
+    cout << "Masukan Alamat =";
+    // Masukan Variable Alamat ;
+    cin >> alamat;
+    // Tampilkan Pesan Di Command Line
+    cout << "Masukan Jenis Kelamin (PR/JK) = ";
+    // Input Variable JK
+    cin >> jk;
+    // Tampilkan Semua Yang sudah di masukan dalam Variable
+    cout << "Nama saya " + name + "\n";
+    cout << "Alamat saya " + alamat + "\n";
+    cout << "Jenis Kelamain Saya" + jk; 
+}
+
 int main()
 {
-    
+    biodata();
 }
